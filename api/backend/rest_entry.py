0320a14f74fd4ab167ca735488e24b6245bc9cb3
Jasmine O'Brien <jasminemarie0205@gmail.com>
2026-04-15 22:54:59 -0400

    Register nutrition blueprint in create_app function

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -6,6 +6,7 @@ import logging
 from backend.db_connection import init_app as init_db
 from backend.simple.simple_routes import simple_routes
 from backend.ngos.ngo_routes import ngos
+from backend.nutrition.nutrition_routes import nutrition
 
 
 def create_app():
@@ -37,5 +38,5 @@ def create_app():
     app.logger.info("create_app(): registering blueprints")
     app.register_blueprint(simple_routes)
     app.register_blueprint(ngos, url_prefix="/ngo")
-
+    app.register_blueprint(nutrition, url_prefix="/nutrition")
     return app
